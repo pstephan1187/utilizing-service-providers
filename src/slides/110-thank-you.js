41288404537b7c1249b32af24c4ee0735efdbf40
Patrick Stephan <pstephan1187@gmail.com>
2020-09-20 23:34:14 -0500

    Added online link

--- src/slides/110-thank-you.js
+++ src/slides/110-thank-you.js
@@ -2,15 +2,19 @@ import React from 'react';
 import {
   Heading,
   Slide,
+  Link
 } from 'spectacle';
 
 export default () => (
   <Slide key="thank-you" backgroundColor="tertiary">
-    <Heading fontSize="18rem" color="light" margin="0px" padding="0px" marginTop="40px">
+    <Heading fontSize="16rem" color="light" margin="0px" padding="0px">
       Thank
     </Heading>
-    <Heading fontSize="18rem" color="light" margin="0px" padding="0px">
+    <Heading fontSize="16rem" color="light" margin="0px" padding="0px">
       You!
     </Heading>
+    <Link color="light" textAlign="center" href="http://utilizing-service-providers.patrickstephan.me" target="_blank" rel="noopener noreferrer">
+      https://utilizing-service-providers.patrickstephan.me
+    </Link>
   </Slide>
 );
